0ce61580c8623bc469438383c202229ce006e9cc
Evans Ochieng <evansochieng121@gmail.com>
2022-08-12 18:03:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+// Function declaration with a default argument
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Hoisting
+// Calling a function before declaration. Works with declaration only
+// Note: Function expressions are not hoisted
+
+// Defining a function using function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// IIFE - Immediately-Invoked Function Expression
+// Scope chain - Children functions access parents' scope variables
+
+function wrapAdjective(flair = '*'){
+    return function(description = 'special'){
+        return `You are ${flair}${description}${flair}!`;
+    };
+};
